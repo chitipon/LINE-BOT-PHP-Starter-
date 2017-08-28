cdbe778788d74dcd5d552d1695169c57254f0dae
chitipon <31399644+chitipon@users.noreply.github.com>
2017-08-28 15:32:00 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'AEyu0dKlsCyILTOlbvLqpCN1KWtqt+S9msUUJWr7tVI8w28DkYz4M8v8kNQgQLFdGck3mGeiV0TDtby+0m4Q9c224RCOTGpxlC/wVfpf13JY2Y/a9oONGlkbiqAvEd0gggKXrUNdjMfcKtNJXXgdpwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
